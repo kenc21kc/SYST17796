61f2c365726ed8c52b45a3a3d14916081f66bf86
Ken <chuak@sheridancollege.ca>
2022-01-25 02:12:46 -0500

    my first commit

---
+++ Shapes/src/shapes/Shapes.java
@@ -0,0 +1,17 @@
+package shapes;
+
+/**
+ * This class +++Insert class description here+++
+ * 
+ *@author Kenneth Chua
+ */
+public class Shapes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
