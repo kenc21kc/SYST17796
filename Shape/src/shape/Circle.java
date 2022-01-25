93dd06716997c66ec5eb644eecc9493c8bbe6018
Ken <chuak@sheridancollege.ca>
2022-01-25 02:27:18 -0500

    my first commit

---
+++ Shape/src/shape/Circle.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shape;
+
+/**
+ *
+ * @author Ken_C21
+ */
+public class Circle {
+    int radius;
+    int size;
+}
---
+++ Shape/src/shape/Shape.java
@@ -0,0 +1,17 @@
+package shape;
+
+/**
+ * This class +++Insert class description here+++
+ * 
+ *@author Kenneth Chua
+ */
+public class Shape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
